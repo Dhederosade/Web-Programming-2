a56eee1bb523383cf8a4e84bf359da88b3021507
Dhederosade <53941700+Dhederosade@users.noreply.github.com>
2020-10-24 16:34:04 +0700

    Add files via upload

---
+++ Praktikum Pertemuan/P1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama Saya Revan Febriansyah <br>
+            Saya Tinggal di daerah Kabubaten Tegal<br>
+            Umurku 20 tahun";
+    }
+}
---
+++ Praktikum Pertemuan/P4/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Revan</li>
+        <li>Nama Belakang : Febriansyah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Prum.Griya Santika Blok O No 44</li> 
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tegal</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Musik</li>
+        <li>Bersepeda</li>
+    </ul>
+</section>
